6050f36a7971192b66eee2ec64980141850bf218
unm14512022c41 <145691688+unm14512022c41@users.noreply.github.com>
2023-09-25 18:16:32 +0530

    Add files via upload

---
+++ challenge 3/main-6.py
@@ -0,0 +1,40 @@
+'''
+Implement a fwnction called sort students that takes a list of student objects as input and sarts the 
+list based on their CGPA (Cumulative Grade Point Average) in descending order.Each student object 
+has the following attributes name (string), roll_number (string), and cgpa (float).Test the function 
+with different input lists of students.
+'''
+
+class Student:
+
+  def __init__(self,name,roll_number,cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(Student_list):
+  #sort the list of students in descendind order of CGPA
+  sorted_student = sorted(student_list,
+                          key=lambda student: student.cgpa,
+                          reverse=True)
+  # syntax_lambda args:exp
+  return sorted_students
+
+
+  # Example usage:
+  students = [
+    student("Hari","A123",7.8),
+    student("Srikanth","A124",8.9),
+    student("Saumya","A125",9.1),
+    student("Mahindhar","A126",9.9)
+  ]
+
+  
+  sorted_students = sort_students(students)
+
+  # print the sorted list of students
+  for student in sorted_students:
+    print("Name:{}, Roll Number: {}, CGPA: {}".format(student.name,
+    student.roll_number,
+                                                      student.cgpa))
